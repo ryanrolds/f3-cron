75885f8aa485da9236e05ca8cc49a6fa39b9235d
Ryan R. Olds <ryanrolds@gmail.com>
2024-06-01 15:30:19 -0700

    Work on upgrading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'xfra35/f3-cron',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3b45902591886a90b4bc8aefa1c68e414fe305bf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => 'dev-redis_tls_and_username',
+            'version' => 'dev-redis_tls_and_username',
+            'reference' => 'de6b41e7a42653a030c30465181ca820bd462480',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'xfra35/f3-cron' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3b45902591886a90b4bc8aefa1c68e414fe305bf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
